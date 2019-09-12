5f50254c63762dafd2072414a0df82b5a4d4d828
Wiktor Mazur <wiktormazur1@gmail.com>
2019-09-12 10:18:20 +0200

    More usefull button aligment

--- gui/src/main.py
+++ gui/src/main.py
@@ -1,3 +1,5 @@
+import sys
+
 from PySide2.QtCore import QTimer
 from PySide2.QtCore import Qt
 
@@ -5,11 +7,50 @@ from PySide2.QtWidgets import QHBoxLayout
 from PySide2.QtWidgets import QPushButton
 from PySide2.QtWidgets import QVBoxLayout
 from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QLabel
 
 import clipboard
 
 from table import Table
 from engine import get_instance_session
+
+class BottomButtons(QWidget):
+    def __init__(self, table):
+        QWidget.__init__(self)
+
+        self.table = table
+        self.layout = QHBoxLayout()
+
+        self.copy_button = QPushButton("&Copy", self)
+        self.reset_button = QPushButton("&Reset", self)
+        self.close_button = QPushButton("&Close", self)
+
+        self.layout.addWidget(self.copy_button)
+        self.layout.addWidget(self.reset_button)
+        self.layout.addWidget(self.close_button)
+        self.setLayout(self.layout)
+
+        self.copy_button.clicked.connect(self.copy)
+        self.reset_button.clicked.connect(self.reset)
+        self.close_button.clicked.connect(self.close)
+
+        self.copy_button.setObjectName('BottomButtons')
+        self.reset_button.setObjectName('BottomButtons')
+        self.close_button.setObjectName('BottomButtons')
+
+    def copy(self):
+        clip = "AO Damage Meter: \n"
+        for i in range(self.table.rowCount()):
+            clip += '{}. \t Player: {} \t Damage: {} \t DPS: {}'.format(i+1, self.table.item(i, 0).text(
+            ), self.table.item(i, 1).text(), self.table.item(i, 2).text())
+            clip += "\n"
+        clipboard.copy(clip)
+
+    def reset(self):
+        self.table.fill([])
+
+    def close(self):
+        sys.exit(0)
 
 class MainWidget(QWidget):
     def __init__(self):
@@ -17,23 +58,22 @@ class MainWidget(QWidget):
 
         self.mouse_pos = None
 
-        self.copy_button = QPushButton("&Copy stats", self)
-        self.refresh_button = QPushButton("&Refresh stats", self)
+        self.phoneLabel = QLabel("Damage: Current instance", self)
+
         self.table = Table()
+        self.bottom_buttons = BottomButtons(self.table)
 
         self.layout = QVBoxLayout()
+        self.layout.addWidget(self.phoneLabel)
         self.layout.addWidget(self.table)
-        self.layout.addWidget(self.copy_button)
-        self.layout.addWidget(self.refresh_button)
+        self.layout.addWidget(self.bottom_buttons)
         self.setLayout(self.layout)
 
         self.table.fill(get_instance_session())
-        self.copy_button.clicked.connect(self.copy)
-        self.refresh_button.clicked.connect(self.clear)
 
         timer = QTimer(self)
         timer.timeout.connect(lambda: self.table.fill(get_instance_session()))
-        timer.start(5000)
+        timer.start(500)
 
     def mousePressEvent(self, event):
         self.mouse_pos = event.pos()
@@ -44,14 +84,3 @@ class MainWidget(QWidget):
             newpos = self.pos() + diff
 
             self.move(newpos)
-
-    def copy(self):
-        clip = "AO Damage Meter: \n"
-        for i in range(self.table.rowCount()):
-            clip += '{}. \t Player: {} \t Damage: {} \t DPS: {}'.format(i+1, self.table.item(i, 0).text(
-            ), self.table.item(i, 1).text(), self.table.item(i, 2).text())
-            clip += "\n"
-        clipboard.copy(clip)
-
-    def clear(self):
-        self.table.fill([])
