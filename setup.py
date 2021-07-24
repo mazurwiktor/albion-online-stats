a5ea89730c28700fbd21f6867b014358d3f757ad
Wiktor Mazur <wiktormazur1@gmail.com>
2021-07-24 14:10:47 +0200

    More detailed setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from os import path
 from setuptools import setup, find_packages
 from src.utils import version
 
@@ -9,15 +10,23 @@ install_requires = [
     "pyaoaddons==0.2.7.dev"
 ]
 
-setup(
-    name='aostats',
-    version=version.get_version(),
-    packages=find_packages('.', include=('src*')),
-    package_data={'': ['**/*.png']},
-    include_package_data=True,
-    install_requires=install_requires,
-    entry_points={
-        'console_scripts': [
-            'aostats = src.__main__:run'
-        ]
-    })
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+    setup(
+        name='aostats',
+        author="Wiktor Mazur",
+        version=version.get_version(),
+        author_email="wiktormazur1@gmail.com",
+        url="https://github.com/mazurwiktor/albion-online-stats",
+        long_description=long_description,
+        packages=find_packages('.', include=('src*')),
+        package_data={'': ['**/*.png']},
+        include_package_data=True,
+        install_requires=install_requires,
+        entry_points={
+            'console_scripts': [
+                'aostats = src.__main__:run'
+            ]
+        })
